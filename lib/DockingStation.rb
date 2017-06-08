8aa02d02f44de7e573f4764377913c81eac5638e
Alistair Kung <alistaircckung@gmail.com>
2017-06-08 16:11:13 +0100

    begun setup of initialization defaults

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -2,11 +2,12 @@ require_relative 'Bike.rb'
 
 class DockingStation
 
-  attr_accessor :bikes
+  attr_accessor :bikes, :capacity
 
  DEFAULT_CAPACITY = 20
 
-  def initialize
+  def initialize(capacity)
+    @capacity = capacity
     @bikes = [Bike.new]
   end
 
--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -1,9 +1,18 @@
+require 'DockingStation.rb'
+
 describe DockingStation do
-
-
-
+  subject {described_class.new(25)}
   it { is_expected.to respond_to(:release_bike) }
   it { is_expected.to respond_to(:dock).with(1).argument }
+
+  describe "attributes" do
+    describe "capacity" do
+      it "can be set by the user upon initialization" do
+        dock = DockingStation.new(25)
+        expect(dock.capacity).to eq(25)
+      end
+    end
+  end
 
   describe "#release_bike" do
 
